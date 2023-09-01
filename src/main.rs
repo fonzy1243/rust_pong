bb15236c63546c7d2fab5b5b6d29a0589eb60595
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-09-01 19:30:29 +0800

    Added collisions and scoring.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use ggez::input::keyboard::KeyCode;
 use ggez::*;
+use rand::prelude::*;
+use std::f32::consts::PI;
 
 // define game window resolution (width x length)
 const SCREEN_SIZE: (f32, f32) = (800., 600.);
@@ -11,13 +13,16 @@ const BALL_SIZE: (f32, f32) = (8., 8.);
 const PADDLE_SIZE: (f32, f32) = (8., 45.);
 
 // define paddle offset from screen side
-const PADDLE_OFFSET: f32 = 75.;
+const PADDLE_OFFSET: f32 = 55.;
 
 // define paddle speed
 const PADDLE_SPEED: f32 = 2.5;
 
 // target fps for physics
 const TARGET_FPS: u32 = 165;
+
+// define default horizontal ball velocity
+const BALL_X_VEL: f32 = 1.5;
 
 fn main() -> GameResult {
     let (ctx, event_loop) = ContextBuilder::new("Pong", "fonzy")
@@ -34,8 +39,6 @@ fn main() -> GameResult {
 enum Direction {
     Up,
     Down,
-    Left,
-    Right,
     None,
 }
 
@@ -100,8 +103,6 @@ impl Player {
                     self.paddle.y = SCREEN_SIZE.1 - PADDLE_SIZE.1
                 }
             }
-            Direction::Left => (),
-            Direction::Right => (),
             Direction::None => (),
         }
     }
@@ -109,10 +110,16 @@ impl Player {
 
 struct Ball {
     ball: ggez::graphics::Rect,
+    velocity: ggez::glam::Vec2,
 }
 
 impl Ball {
     fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let rand_y: f32 = rng.gen_range(-1.0..=1.0);
+
+        let velocity = ggez::glam::Vec2::new(BALL_X_VEL, rand_y);
+
         Self {
             ball: ggez::graphics::Rect::new(
                 SCREEN_SIZE.0 / 2. + BALL_SIZE.0 / 2.,
@@ -120,6 +127,7 @@ impl Ball {
                 BALL_SIZE.0,
                 BALL_SIZE.1,
             ),
+            velocity,
         }
     }
 
@@ -128,6 +136,51 @@ impl Ball {
             &ggez::graphics::Quad,
             ggez::graphics::DrawParam::new().dest_rect(self.ball),
         );
+    }
+
+    fn update(&mut self, players: (&mut Player, &mut Player)) {
+        // check if ball collided with the top or bottom of the screen
+        if self.ball.y <= 0. || self.ball.y >= SCREEN_SIZE.1 - BALL_SIZE.1 {
+            self.velocity.y = -self.velocity.y;
+        }
+
+        if self.ball.x <= 0. {
+            // update player 1 score
+            players.0.score += 1;
+
+            self.ball.x = SCREEN_SIZE.0 / 2. + BALL_SIZE.0 / 2.;
+            self.ball.y = SCREEN_SIZE.1 / 2. - BALL_SIZE.1 / 2.;
+
+            let mut rng = rand::thread_rng();
+            let rand_y = rng.gen_range(-1.0..=1.0);
+
+            self.velocity = ggez::glam::Vec2::new(-BALL_X_VEL, rand_y);
+        } else if self.ball.x >= SCREEN_SIZE.0 - BALL_SIZE.0 {
+            players.1.score += 1;
+
+            self.ball.x = SCREEN_SIZE.0 / 2. + BALL_SIZE.0 / 2.;
+            self.ball.y = SCREEN_SIZE.1 / 2. - BALL_SIZE.1 / 2.;
+
+            let mut rng = rand::thread_rng();
+            let rand_y = rng.gen_range(-1.0..=1.0);
+
+            self.velocity = ggez::glam::Vec2::new(BALL_X_VEL, rand_y);
+        }
+
+        self.ball.x += self.velocity.x;
+        self.ball.y += self.velocity.y;
+    }
+
+    fn check_collision(&mut self, paddle: ggez::graphics::Rect) {
+        if self.ball.overlaps(&paddle) {
+            let offset = (self.ball.y + BALL_SIZE.1 - paddle.y) / (PADDLE_SIZE.1 + BALL_SIZE.1);
+            let phi = 0.25 * PI * (2. * offset - 1.);
+
+            let speed = self.velocity.length();
+
+            self.velocity.x *= -1.;
+            self.velocity.y = speed * phi.sin();
+        }
     }
 }
 
@@ -179,6 +232,11 @@ impl ggez::event::EventHandler<GameError> for State {
 
             self.player1.update();
             self.player2.update();
+            self.ball.update((&mut self.player1, &mut self.player2));
+
+            // check for ball-paddle collisions
+            self.ball.check_collision(self.player1.paddle);
+            self.ball.check_collision(self.player2.paddle);
         }
 
         Ok(())
